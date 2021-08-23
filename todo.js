0ecd9c17a553373e7f6c82f7dbc97cf9344b49a6
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-08-23 23:59:38 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", toDos);
+}
+
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -23,6 +29,7 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
 }
 
