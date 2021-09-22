ae8d0a44fa98888ee174b9a579747d008dd96673
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-09-22 19:30:54 +0900

    쇼핑 리스트 만들기

--- jsbasicwithel/101 broswer/chapter 1/coordinates/main.js
+++ jsbasicwithel/101 broswer/chapter 1/coordinates/main.js
@@ -2,17 +2,23 @@ const vertical = document.querySelector(".vertical");
 const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
 
-document.addEventListener("mousemove", (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
-  console.log(`${x},${y}`);
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${y}px`;
-  tag.style.top = `${y}px`;
-  tag.innerHTML = `${x}px,${y}px`;
+  document.addEventListener("mousemove", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log(`${x},${y}`);
+
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px , ${
+      y - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${x}px , ${y}px)`;
+    tag.innerHTML = `${x}px,${y}px`;
+  });
 });
