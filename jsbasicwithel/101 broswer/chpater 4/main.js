d7cb604cca2bf56d19e51b8b3f638747744b2286
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-09-28 22:42:41 +0900

    쇼핑 리스트 remark

--- jsbasicwithel/101 broswer/chpater 4/main.js
+++ jsbasicwithel/101 broswer/chpater 4/main.js
@@ -1,6 +1,12 @@
 const items = document.querySelector(".items");
 const input = document.querySelector(".footer__input");
 const addBtn = document.querySelector(".footer__button");
+const form = document.querySelector(".new-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  onAdd();
+});
 
 function onAdd() {
   const text = input.value;
@@ -14,41 +20,27 @@ function onAdd() {
   input.value = "";
   input.focus();
 }
-
+let id = 0;
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = ' <i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+        <div class="item">
+          <span class="item__name">${text}</span>
+          <button class="item__delete" >
+              <i class="fas fa-trash-alt" data-id=${id}></i>
+          </button>
+        </div>
+        <div class="item__divider"></div>`;
+  id++;
   return itemRow;
 }
 
-addBtn.addEventListener("click", () => {
-  onAdd();
-});
-
-input.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
-    onAdd();
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
