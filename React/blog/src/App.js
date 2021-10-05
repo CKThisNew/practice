cbecd45c964af1eb9727c74e53d43d795c46e313
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-10-05 19:18:24 +0900

    다시 복구

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -5,29 +5,23 @@ import "./App.css";
 
 function App() {
   let [글제목, 글제목변경] = useState([
-    "아라동 여행지 추천",
-    "삼양 여행지 추천",
-    "함덕 여행지 추천",
+    "전라도 여행",
+    "경상도 여행",
+    "경기도 여행",
   ]);
 
-  let [따봉, 따봉변경] = useState(0);
-  let [modal, modal변경] = useState(false);
+  let [추천, 추천변경] = useState(0);
+
+  let [모달, 모달변경] = useState(false);
+
   let [누른제목, 누른제목변경] = useState(0);
-
-  function 제목바꾸기() {
-    let newArray = [...글제목];
-    newArray[0] = "삼양 여행지 추천";
-
-    글제목변경(newArray);
-  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-
-      {글제목.map(function (a, i) {
+      {글제목.map((a, i) => {
         return (
           <div className="list">
             <h3
@@ -38,63 +32,39 @@ function App() {
               {a}
               <span
                 onClick={() => {
-                  따봉변경(i + 1);
+                  추천변경(추천 + 1);
                 }}
               >
-                ❤️
+                👍🏻
               </span>
-              {따봉}
+              <span>{추천}</span>
             </h3>
-            <p> 10월 3일 발행</p>
+            <span>10월 5일 발행</span>
             <hr />
           </div>
         );
       })}
+
       <button
         onClick={() => {
-          누른제목변경(0);
+          모달변경(!모달);
         }}
       >
-        버튼1
+        열고닫기
       </button>
-      <button
-        onClick={() => {
-          누른제목변경(1);
-        }}
-      >
-        버튼2
-      </button>
-      <button
-        onClick={() => {
-          누른제목변경(2);
-        }}
-      >
-        버튼3
-      </button>
-      <button
-        onClick={() => {
-          modal변경(!modal);
-        }}
-      >
-        열고닫는버튼
-      </button>
-      {modal === true ? (
+      {모달 === true ? (
         <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
       ) : null}
     </div>
   );
 }
 
-//반복출현하는 HTML 덩어리들을 컴포넌트로 만들어주면좋다
-// 자주 변경되는 HTML UI들
-// 다른 페이지로 만들 때 컴포넌트 사용
-
 function Modal(props) {
   return (
     <div className="modal">
       <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
-      <p>상세내용</p>
+      <p>내용</p>
     </div>
   );
 }
