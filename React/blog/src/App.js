3c20c0e2032ca31224b8001b3caab1dac5a16aab
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-10-05 18:30:23 +0900

    props 응용

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -11,8 +11,8 @@ function App() {
   ]);
 
   let [따봉, 따봉변경] = useState(0);
-
   let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
 
   function 제목바꾸기() {
     let newArray = [...글제목];
@@ -26,41 +26,19 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
-      <div className="list">
-        <h3>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}
-          >
-            ❤️
-          </span>
-          {따봉}
-        </h3>
-        <p> 10월 3일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3>{글제목[1]}</h3>
-        <p> 10월 3일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3>{글제목[2]}</h3>
-        <p> 10월 3일 발행</p>
-        <hr />
-      </div>
 
-      {글제목.map(function (a) {
+      {글제목.map(function (a, i) {
         return (
           <div className="list">
-            <h3>
+            <h3
+              onClick={() => {
+                누른제목변경(i);
+              }}
+            >
               {a}
               <span
                 onClick={() => {
-                  따봉변경(따봉 + 1);
+                  따봉변경(i + 1);
                 }}
               >
                 ❤️
@@ -72,7 +50,27 @@ function App() {
           </div>
         );
       })}
-
+      <button
+        onClick={() => {
+          누른제목변경(0);
+        }}
+      >
+        버튼1
+      </button>
+      <button
+        onClick={() => {
+          누른제목변경(1);
+        }}
+      >
+        버튼2
+      </button>
+      <button
+        onClick={() => {
+          누른제목변경(2);
+        }}
+      >
+        버튼3
+      </button>
       <button
         onClick={() => {
           modal변경(!modal);
@@ -80,7 +78,9 @@ function App() {
       >
         열고닫는버튼
       </button>
-      {modal === true ? <Modal 글제목={글제목}></Modal> : null}
+      {modal === true ? (
+        <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
+      ) : null}
     </div>
   );
 }
@@ -92,7 +92,7 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h2>{props.글제목[2]}</h2>
+      <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
