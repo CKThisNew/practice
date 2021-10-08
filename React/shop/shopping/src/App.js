fcdbb3a25cd5b26c8dc926f562e56fde94804a04
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-10-09 00:13:48 +0900

    export사용해서 state 만들기
    내일 map 응용하기

--- React/shop/shopping/src/App.js
+++ React/shop/shopping/src/App.js
@@ -1,9 +1,13 @@
-import Reat from "react";
+import Reat, { useState } from "react";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
-import logo from "./logo.svg";
 import "./App.css";
+import Data from "./data.js";
+import image from "./shoesNum.js";
 
 function App() {
+  let [shoes, shoes변경] = useState(Data);
+  let [신발사진, 신발사진변경] = useState(image);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
@@ -33,11 +37,33 @@ function App() {
       </Navbar>
       <div className="background">
         <h2>이거 최고</h2>
-        <p>이래도 안산다고?</p>
         <p>
-          <button className="background-button">당!장!</button>
+          이래도 안산다고? 진짜 후회 할텐데? 다시는 안돌아오는 파격세일인데?
+        </p>
+        <p>
+          <button className="background-button">구매</button>
         </p>
       </div>
+
+      <div className="container">
+        <div className="row">
+          <Product shoes={shoes} 신발사진={신발사진} />
+          <Product shoes={shoes} 신발사진={신발사진} />
+          <Product shoes={shoes} 신발사진={신발사진} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className="col-md-4">
+      <img src={props.신발사진[0]} width="100%" />
+      <h4>{props.shoes[0].title}</h4>
+      <p>
+        {props.shoes[0].content} & {props.shoes[0].price}
+      </p>
     </div>
   );
 }
---
+++ React/shop/shopping/src/shoesNum.js
@@ -0,0 +1,5 @@
+export default [
+  "https://codingapple1.github.io/shop/shoes1.jpg",
+  "https://codingapple1.github.io/shop/shoes2.jpg",
+  "https://codingapple1.github.io/shop/shoes3.jpg",
+];
