602b529831c5bf30f023f70838a4d5b3f124abc9
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-10-08 21:35:40 +0900

    css하는중...

--- React/shop/shopping/src/App.js
+++ React/shop/shopping/src/App.js
@@ -1,12 +1,43 @@
+import Reat from "react";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">
-        Primary
-      </button>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="background">
+        <h2>이거 최고</h2>
+        <p>이래도 안산다고?</p>
+        <p>
+          <button className="background-button">당!장!</button>
+        </p>
+      </div>
     </div>
   );
 }
