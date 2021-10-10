72df3079ac6161fb667363d109692a01c0716044
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-10-10 22:06:07 +0900

    라우터(페이지 나누기)기초

--- React/shop/shopping/src/App.js
+++ React/shop/shopping/src/App.js
@@ -1,10 +1,14 @@
 /* eslint-disable */
+// eslint-disable-next-line
 
 import Reat, { useState } from "react";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import "./App.css";
 import Data from "./data.js";
 import image from "./shoesNum.js";
+import Detail from "./Detail.js";
+
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   let [shoes, shoes변경] = useState(Data);
@@ -18,8 +22,12 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Detail">Detail</Link>
+              </Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -37,28 +45,39 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="background">
-        <h2>이거 최고</h2>
-        <p>
-          이래도 안산다고? 진짜 후회 할텐데? 다시는 안돌아오는 파격세일인데?
-        </p>
-        <p>
-          <button className="background-button">구매</button>
-        </p>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <div className="background">
+            <h2>이거 최고</h2>
+            <p>
+              이래도 안산다고? 진짜 후회 할텐데? 다시는 안돌아오는 파격세일인데?
+            </p>
+            <p>
+              <button className="background-button">구매</button>
+            </p>
+          </div>
+          <div className="container">
+            <div className="row">
+              {shoes.map((a, i) => {
+                return <Card shoes={a} 신발사진={신발사진[i]} />;
+              })}
+            </div>
+          </div>
+        </Route>
 
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Product shoes={a} 신발사진={신발사진[i]} key={i} />;
-          })}
-        </div>
-      </div>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+
+        <Route path="/:id">
+          <div>dkdkdkdkdkdkdkdkdk</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-function Product(props) {
+function Card(props) {
   return (
     <div className="col-md-4">
       <img src={props.신발사진} width="100%" />
